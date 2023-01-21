b2cc045532bc935dc5b7c27696153eea29d0a966
Wajd Alloush <Wajd.Alloush@colvengn.com.ar>
2023-01-21 10:21:33 -0300

    add Deploy a Single Web Serve file

---
+++ Terraform/Deploy a Single Web Server/Deploy a Single Web Server.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+ region = "us-east-1"
+}
+
+
+resource "aws_instance" "example" {
+ ami = "ami-00874d747dde814fa"
+ instance_type = "t2.micro"
+ vpc_security_group_ids = [aws_security_group.instance.id]
+
+ user_data = <<-EOF
+ #!/bin/bash
+ echo "Hello, World" > index.html
+ nohup busybox httpd -f -p 8080 &
+ EOF
+ 
+ tags = {
+ Name = "terraform-example"
+ }
+}
+#You need to do one more thing before this web server works add SG
+resource "aws_security_group" "instance" {
+ name = "terraform-example-instance"
+ ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
